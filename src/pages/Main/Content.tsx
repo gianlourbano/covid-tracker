5db9f94e2900a93a58ea161f88a72608306bf2f8
gianlourbano <gianlo.urbano@outlook.it>
2020-10-08 22:50:47 +0200

    error handling

--- src/pages/Main/Content.tsx
+++ src/pages/Main/Content.tsx
@@ -1,106 +1,133 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import { ScrollView, StyleSheet, Text, View } from "react-native"
-import { Button } from "react-native-elements"
+import { Button, Overlay } from "react-native-elements"
 import { useQuery } from "react-query"
 import ContentBlock from "../../components/ContentBlock"
 import axios from "axios"
 import * as Animatable from 'react-native-animatable';
-
 
 const virus = require("../../assets/virus.png")
 const death = require("../../assets/death.png")
 const mask = require("../../assets/mask.png")
 
 export interface SummaryData {
-    type: string,
-    total_cases: string,
-    new_cases: string,
-    total_deaths: string,
-    new_deaths: string,
-    total_recovered: string,
-    active_cases: string,
-    critical_active_cases: string,
-    time: string,
+  type: string,
+  total_cases: string,
+  new_cases: string,
+  total_deaths: string,
+  new_deaths: string,
+  total_recovered: string,
+  active_cases: string,
+  critical_active_cases: string,
+  time: string,
 }
 
 export function numberWithSpaces(x: number) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
-const Content: React.FC<{ navigation: any}> = ({ navigation }) => {
+const Content: React.FC<{ navigation: any }> = ({ navigation }) => {
 
-    const { data, isFetching, refetch } = useQuery("covid", async () => {
-        let { data }: { data: Array<SummaryData> } = await axios.get("https://covid19-scraper-utterlabs.herokuapp.com/api/data")
-        return(data)
-    }, {
-        refetchOnWindowFocus: "always",
-    })
+  const { data, isFetching, isError, refetch } = useQuery("covid", async () => {
+    let { data }: { data: Array<SummaryData> } = await axios.get("https://covid19-scraper-utterlabs.herokuapp.com/api/data")
+    return (data)
+  }, {
+    refetchOnWindowFocus: "always",
+  })
 
-    return(
-        <View style={{marginBottom: 60}}>
-            <Text style={styles.title}>COVID-19 Summary</Text>
-            <ScrollView>
-                <View style={styles.root}>
-                    {data && !isFetching && (
-                        <>
-                            <ContentBlock text="Total Cases" data={data[0].total_cases} icon={virus} />
-                            <ContentBlock text="Total Deaths" data={data[0].total_deaths} icon={death} secondary inverted />
-                            <ContentBlock text="Total Recovered" data={data[0].total_recovered} icon={mask} />
-                        </>
-                    )}  
-                </View>
-                {!isFetching && (
-                    <Animatable.View style={styles.actions} animation="bounceIn" duration={2000} delay={500} useNativeDriver>
-                        <StyledButton title="Update"
-                            func={() => refetch()}
-                        />
-                        <StyledButton title="Daily" func={() => navigation.navigate("Daily", {
-                            data: data
-                        })} />
-                    </Animatable.View>
-                )}
-            </ScrollView>
+  const [isErrorOverlayOpen, setisErrorOverlayOpen] = useState(false)
+
+  useEffect(() => {
+    if (isError) {
+      setisErrorOverlayOpen(true)
+    } else {
+      setisErrorOverlayOpen(false)
+    }
+  }, [isError])
+
+  const toggleOverlay = () => {
+    setisErrorOverlayOpen(!isErrorOverlayOpen)
+  }
+
+  return (
+    <View style={{ marginBottom: 60 }}>
+      <Text style={styles.title}>COVID-19 Summary</Text>
+      <ScrollView>
+        <View style={styles.root}>
+          <Overlay isVisible={isError} onBackdropPress={toggleOverlay}>
+            <>
+              <Text>Error while fetching data!</Text>
+              <StyledButton title="retrieve" func={() => refetch()} />
+            </>
+          </Overlay>
+          <Overlay isVisible={isFetching} animationType="fade">
+            <Text>Fetching data...</Text>
+          </Overlay>
+          {data && !isFetching && (
+            <>
+              <ContentBlock text="Total Cases" data={data[0].total_cases} icon={virus} />
+              <ContentBlock text="Total Deaths" data={data[0].total_deaths} icon={death} secondary inverted />
+              <ContentBlock text="Total Recovered" data={data[0].total_recovered} icon={mask} />
+            </>
+          )}
         </View>
-    )
+        {!isFetching && (
+          <Animatable.View style={styles.actions} animation="bounceIn" duration={2000} delay={500} useNativeDriver>
+            <StyledButton title="Update"
+              func={() => refetch()}
+            />
+            <StyledButton title="Daily" func={() => navigation.navigate("Daily", {
+              data: data
+            })} />
+          </Animatable.View>
+        )}
+      </ScrollView>
+    </View>
+  )
 }
 
-const StyledButton: React.FC<{func: () =>{}, title: string}> = ({func, title}) => {
-    return(
-        <Button
-            containerStyle={styles.button}
-            onPress={func}
-            title={title}
-        />
-    )
+export const StyledButton: React.FC<{ func: () => {}, title: string }> = ({ func, title }) => {
+  return (
+    <Button
+      containerStyle={styles.button}
+      onPress={func}
+      title={title}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        flexGrow: 1,
-        paddingHorizontal: 20,
-        paddingTop: 20,
-    },
-    title: {
-        color: "#57596F",
-        textAlign: "center",
-        fontSize: 35,
-        marginTop: 22,
-    },
-    date: {
-        marginTop: 40
-    },
-    actions: {
-        marginVertical: 40,
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-    },
-    button: {
-        flexGrow: 1,
-        flexShrink: 1,
-        flexBasis: 0,
-        marginHorizontal: 20,
-    },
+  root: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  title: {
+    color: "#57596F",
+    textAlign: "center",
+    fontSize: 35,
+    marginTop: 22,
+  },
+  date: {
+    marginTop: 40
+  },
+  actions: {
+    marginVertical: 40,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  button: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    marginHorizontal: 20,
+  },
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  }
 });
 
 export default Content
--- src/pages/World.tsx
+++ src/pages/World.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
-import { ActivityIndicator, NativeSyntheticEvent, StyleSheet, Text, TextInputChangeEventData, View, ScrollView } from "react-native"
-import { Input } from "react-native-elements"
+import { ActivityIndicator, StyleSheet, Text, View, ScrollView } from "react-native"
+import { Button, Input, Overlay } from "react-native-elements"
 import { useQuery } from "react-query"
 import axios from "axios"
 import ContentBlock from "../components/ContentBlock"
 import { numberWithSpaces } from "./Main/Content"
+
 
 const virus = require("../assets/virus.png")
 const death = require("../assets/death.png")
@@ -12,81 +13,103 @@ const mask = require("../assets/mask.png")
 const novirus = require("../assets/no-virus.png")
 
 interface Response {
-    Country: string,
-    Confirmed: number,
-    Deaths: number,
-    Recovered: number,
-    Active: number,
-    Date: string
+  Country: string,
+  Confirmed: number,
+  Deaths: number,
+  Recovered: number,
+  Active: number,
+  Date: string
 
 }
 
 const World: React.FC = () => {
-    
-    const [text, setText] = useState("")
 
-    const { data, isFetching, refetch } = useQuery(["world", text], async () => {
-        let { data }: {data: Array<Response>} = await axios.get(`https://api.covid19api.com/total/dayone/country/${text}`)
-        data = data.slice(-2)
-        return(data)
-    }, {
-        refetchOnWindowFocus: false,
-        enabled: false
-    })
+  const [text, setText] = useState("")
 
-    const onSubmit = (e: any) => {
-        e.preventDefault()
-        refetch()
-    }
-    
-    return(
-        <View style={styles.root}>
-            <Text style={styles.text}>World Info</Text>
-            <Input
-                value={text}
-                onChangeText={text => setText(text)}
-                onSubmitEditing={onSubmit}
-                placeholder='Search for Country'
-                rightIcon={{ type: "font-awesome-5", name: 'search' }}
-            />
-            <ActivityIndicator size="large" animating={isFetching} />
-            {data && (
-                <ScrollView style={styles.data}>
-                    <Text style={styles.title}>Summary - {data[1].Country}</Text>
-                    <ContentBlock data={numberWithSpaces(data[1].Active)} text="Active Cases" secondary icon={mask} />
-                    <ContentBlock data={numberWithSpaces(data[1].Confirmed)} text="Total Confirmed Cases" inverted icon={virus} />
-                    <ContentBlock data={numberWithSpaces(data[1].Deaths)} text="Total Deaths" secondary icon={death} />
-                    <ContentBlock data={numberWithSpaces(data[1].Recovered)} text="Total Recovered" inverted icon={novirus} />
-                    <Text style={styles.title}>Daily</Text>
-                    <ContentBlock data={numberWithSpaces(data[1].Active - data[0].Active)} text="New Active Cases" secondary icon={mask} />
-                    <ContentBlock data={numberWithSpaces(data[1].Confirmed - data[0].Confirmed)} text="New Confirmed Cases" inverted icon={virus} />
-                    <ContentBlock data={numberWithSpaces(data[1].Deaths - data[0].Deaths)} text="New Deaths" secondary icon={death} />
-                    <ContentBlock data={numberWithSpaces(data[1].Recovered - data[0].Recovered)} text="New Recovered" inverted icon={novirus} />
-                    <Text style={[styles.title, { fontSize: 20 }]}>Last Updaded on {data[1].Date}</Text>
-                </ScrollView>
-            )}
-        </View>
-    )
+  const { data, isFetching, isError, refetch } = useQuery(["world", text], async () => {
+    let { data }: { data: Array<Response> } = await axios.get(`https://api.covid19api.com/total/dayone/country/${text}`)
+    data = data.slice(-2)
+    return (data)
+  }, {
+    refetchOnWindowFocus: false,
+    enabled: false
+  })
+
+  const onSubmit = (e: any) => {
+    e.preventDefault()
+    refetch()
+  }
+
+  const handleError = () => {
+    setText("")
+  }
+
+  return (
+    <View style={styles.root}>
+      <Text style={styles.text}>World Info</Text>
+      <Input
+        value={text}
+        onChangeText={text => setText(text)}
+        onSubmitEditing={onSubmit}
+        placeholder='Search for Country'
+        rightIcon={{ type: "font-awesome-5", name: 'search' }}
+      />
+      <ActivityIndicator size="large" animating={isFetching} />
+      <Overlay isVisible={isError}
+        onBackdropPress={() => { }}
+        overlayStyle={styles.overlay}
+        animationType="fade"
+      >
+        <>
+          <Text>Error!</Text>
+          <Text>This might be a problem with the server, or you mispelled country name</Text>
+          <Button onPress={() => handleError()} 
+                  title="Exit"
+                  containerStyle={{ padding: 10, alignSelf: "flex-end" }} 
+                  />
+        </>
+      </Overlay>
+      {data && (
+        <ScrollView style={styles.data}>
+          <Text style={styles.title}>Summary - {data[1].Country}</Text>
+          <ContentBlock data={numberWithSpaces(data[1].Active)} text="Active Cases" secondary icon={mask} />
+          <ContentBlock data={numberWithSpaces(data[1].Confirmed)} text="Total Confirmed Cases" inverted icon={virus} />
+          <ContentBlock data={numberWithSpaces(data[1].Deaths)} text="Total Deaths" secondary icon={death} />
+          <ContentBlock data={numberWithSpaces(data[1].Recovered)} text="Total Recovered" inverted icon={novirus} />
+          <Text style={styles.title}>Daily</Text>
+          <ContentBlock data={numberWithSpaces(data[1].Active - data[0].Active)} text="New Active Cases" secondary icon={mask} />
+          <ContentBlock data={numberWithSpaces(data[1].Confirmed - data[0].Confirmed)} text="New Confirmed Cases" inverted icon={virus} />
+          <ContentBlock data={numberWithSpaces(data[1].Deaths - data[0].Deaths)} text="New Deaths" secondary icon={death} />
+          <ContentBlock data={numberWithSpaces(data[1].Recovered - data[0].Recovered)} text="New Recovered" inverted icon={novirus} />
+          <Text style={[styles.title, { fontSize: 20 }]}>Last Updaded on {data[1].Date}</Text>
+        </ScrollView>
+      )}
+    </View>
+  )
 }
 
-const styles=StyleSheet.create({
-    root: {
-        paddingHorizontal: 20
-    },
-    text: {
-        color: "#57596F",
-        fontSize: 35,
-        textAlign: "center"
-    },
-    title: {
-        color: "#57596F",
-        textAlign: "center",
-        fontSize: 35
-    },
-    data: {
-        flexGrow: 1,
-        marginBottom: 170,
-    }
+const styles = StyleSheet.create({
+  root: {
+    paddingHorizontal: 20
+  },
+  text: {
+    color: "#57596F",
+    fontSize: 35,
+    textAlign: "center"
+  },
+  title: {
+    color: "#57596F",
+    textAlign: "center",
+    fontSize: 35
+  },
+  data: {
+    flexGrow: 1,
+    marginBottom: 170,
+  },
+  overlay: {
+    marginHorizontal: 20,
+    padding: 20,
+  }
 })
 
 export default World
